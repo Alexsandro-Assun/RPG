0911c4bfbc78bd1aaa6db1f5a88ea2ff7b0c66ab
Alexsandro-Assun <alexsandro.15_sp@hotmail.com>
2021-04-29 21:29:54 -0300

    jogo finalizado v1.0

--- Rpg/src/CriandoRpg/Jogo.java
+++ Rpg/src/CriandoRpg/Jogo.java
@@ -1,71 +1,206 @@
 package CriandoRpg;
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class Jogo {
 
 	public static void main(String[] args) {
 
-		int op1, op2;
+		int op1, ataque, magia, critico,op2=0;
 		Scanner ler = new Scanner(System.in);
+		Random r = new Random();
 		
 		System.out.println("Digite o nome do jogador ");
-		
-		Player p1 = new Player(500, 4, 10, 15, 5, 1.5);
+
+		Player p1 = new Player(900, 4, 3, 15, 5, (float) 1.5);
 		p1.nome = ler.nextLine();
 		p1.visivel();
 		p1.mostrarStatus();
-		
-		Player cpu = new Player(500, 4, 10, 15, 5, 2.0 );
+
+
+		Player cpu = new Player(1000, 5, 3, 15, 5, (float) 2.0);
 		cpu.nome = "CPU";
-
-		System.out.println("     Habilidades");
-		System.out.println("=======================");
-		System.out.println("|  ESCOLHA UMA OPÇÃO: |");
-		System.out.println("|  1) Ataque Físico   |");
-		System.out.println("|  2) Magia           |");
-		System.out.println("|  3) Crítico         |");
-		System.out.println("=======================");
-
-		op1 = ler.nextInt();
-
-		switch(op1) {
-		case 1:{
-			System.out.println("    Ataque Físico");
-			
-			System.out.println("=======================");
-			System.out.println("|  1) Santouryuu      |");
-			System.out.println("=======================");
-
-		} break;
 		
-		
-			
-		case 2:{
-			System.out.println("	   Magias");
-			
-			System.out.println("=======================");
-			System.out.println("|  1) Blizzard        |");
-			System.out.println("=======================");
-
-
-		} break;
-		
-		
-		
-		case 3:{
-			System.out.println("      Crítico ");
-
-			
-			System.out.println("=======================");
-			System.out.println("| 1) Bakuhatsu        |");
-			System.out.println("=======================");
-
-		} break;
-			
-			
+		if(p1.nome.equals(cpu.nome)) {
+			cpu.setHp(0);
 			
 		}
+
+		while (p1.getHp() > 0 && cpu.getHp() > 0) {
+			
+			
+			System.out.println("     Habilidades       ");
+			System.out.println("=======================");
+			System.out.println("|  ESCOLHA UMA OPÇÃO: |");
+			System.out.println("|  1) Ataque Físico   |");
+			System.out.println("|  2) Magia           |");
+			System.out.println("|  3) Crítico         |");
+			System.out.println("=======================");
+
+			op1 = ler.nextInt();
+
+			switch (op1) {
+
+			case 1: {
+				System.out.println("    Ataque Físico      ");
+
+				System.out.println("=======================");
+				System.out.println("|  1) Santouryuu      |");
+				System.out.println("|  2) TotsukaTsurugi  |");
+				System.out.println("=======================");
+				ataque = ler.nextInt();
+
+				switch (ataque) {
+
+				case 1: {
+					cpu.Santouryuu();
+					System.out.println("\nCPU HP restante :" + cpu.getHp() + "\n");
+				} break;
+
+				case 2: {
+					cpu.TotsukaTsurugi();
+					System.out.println("\nCPU HP restante :" + cpu.getHp() + "\n");
+				} break;
+
+				default:
+					System.out.println("\nOpção inválida\n");
+
+				}
+
+			}
+				break;
+
+//------------------ ------------magias--------------------------------------------
+			case 2: {
+				System.out.println("	 Magias            ");
+
+				System.out.println("=======================");
+				System.out.println("|  1) Blizzard        |");
+				System.out.println("|  2) FaiyaaArashi    |");
+				System.out.println("|  3) Raitou          |");
+				System.out.println("=======================");
+				magia = ler.nextInt();
+
+				switch (magia) {
+				case 1: {
+					cpu.Blizzard();
+					System.out.println("\nCPU HP restante :" + cpu.getHp() + "\n");
+
+				}
+					break;
+
+				case 2: {
+					cpu.FaiyaaArashi();
+					System.out.println("\nCPU HP restante :" + cpu.getHp() + "\n");
+
+				}
+					break;
+
+				case 3: {
+					cpu.Raitou();
+					System.out.println("\nCPU HP restante :" + cpu.getHp() + "\n");
+
+				} break ;
+				
+				default:
+					System.out.println("\nOpção inválida\n");
+				
+				}
+			}
+				break;
+
+//------------------ ------------critico--------------------------------------------
+
+			case 3: {
+				System.out.println("      Crítico          ");
+
+				System.out.println("=======================");
+				System.out.println("| 1) Bakuhatsu        |");
+				System.out.println("=======================");
+
+				critico = ler.nextInt();
+
+				switch (critico) {
+
+				case 1:
+					cpu.critico = p1.critico;
+					cpu.Bakuhatsu();
+					System.out.println("\nCPU HP restante :" + cpu.getHp() + "\n");
+					break;
+
+				default:
+					System.out.println("\nOpção inválida\n");
+
+					break;
+
+				}
+
+			}
+
+				break;
+
+			default: {
+				System.out.println("\nOpção inválida\n");
+			}
+
+			}
+			System.out.println("=======================");
+			System.out.println("|Turno do jogador: "+cpu.nome+"|");
+			System.out.println("=======================\n");
+
+			
+			switch(op2+r.nextInt(5)) {
+				case 1:{
+					System.out.print(cpu.nome+" usou ");
+					p1.Blizzard();
+					System.out.println("\n"+p1.nome+" HP restante :" + p1.getHp() + "\n");
+				} break;
+				
+				case 2:{
+					System.out.print(cpu.nome+" usou ");
+					p1.Santouryuu();
+					System.out.println("\n"+p1.nome+" HP restante :" + p1.getHp() + "\n");
+				} break;
+				
+				case 3:{
+					System.out.print(cpu.nome+" usou ");
+					p1.TotsukaTsurugi();
+					System.out.println("\n"+p1.nome+" HP restante :" + p1.getHp() + "\n");
+				} break;
+				
+				case 4:{
+					System.out.print(cpu.nome+" usou ");
+					p1.FaiyaaArashi();
+					System.out.println("\n"+p1.nome+" HP restante :" + p1.getHp() + "\n");
+				} break;
+				
+				case 5:{
+					p1.critico = cpu.critico;
+					System.out.print(cpu.nome+" usou ");
+					p1.Bakuhatsu();
+					System.out.println("\n"+p1.nome+" HP restante :" + p1.getHp() + "\n");
+				} break;
+				
+				case 6:{
+					System.out.print(cpu.nome+" usou ");
+					p1.Raitou();
+					System.out.println("\n"+p1.nome+" HP restante :" + p1.getHp() + "\n");
+				} break;
+				
+				default:{
+					System.out.print(cpu.nome+" usou ");
+					p1.Raitou();
+					System.out.println("\n"+p1.nome+" HP restante :" + p1.getHp() + "\n");
+				} break;
+			}
+		} 
+		if(p1.getHp() > 0) {
+			System.out.println("### VOCÊ GANHOU ###");
+		} else {
+			System.out.println("### VOCÊ PERDEU ###");
+		}
+			System.out.println("### FIM DE JOGO ###");
 	}
 
 }
--- Rpg/src/CriandoRpg/Player.java
+++ Rpg/src/CriandoRpg/Player.java
@@ -1,114 +1,281 @@
 package CriandoRpg;
 
 public class Player {
-	
+
 	public String nome;
-	private float HP;
+	private float hp;
 	private int armadura;
 	private int resisteciaMagica;
 	private int danoFisico;
 	private int danoMagico;
-	private double critico;
+	public float critico;
 	public boolean ocultar;
-	
-	public Player(float hp, int arm, int mr, int ad, int ap, double crt) {
-		//nome();
-		this.HP = hp;
+
+	// Skills
+	private int santouryuu = 25;
+	private float bakuhatsu = 11;
+	private int totsukaTsurugi = 19;
+	private float faiyaaArashi = (float) 3.5;
+	private int blizzard = 35;
+	private int raitou = 8;
+	private int ac = 1 ;
+	private int cont = 5;
+
+
+	// metodos dos players
+	public Player(float hp, int arm, int mr, int ad, int ap, float crt) {
+		this.hp = hp;
 		this.armadura = arm;
 		this.resisteciaMagica = mr;
 		this.danoFisico = ad;
 		this.danoMagico = ap;
 		this.critico = crt;
-		
+
 	}
 
-	public void status()
-	{
-		System.out.println("Status base do jogador: "+this.nome);
-		System.out.println("Vida: "+this.HP);
-		System.out.println("Armadura: "+this.armadura);
-		System.out.println("Resistência Mágica: "+this.resisteciaMagica);
-		System.out.println("Dano Físico: "+this.danoFisico);
-		System.out.println("Dano Mágico: "+this.danoMagico);
-		System.out.println("Crítico: "+this.critico+"\n");
-		
+	public void status() {
+		System.out.println("Status base do jogador: " + this.nome);
+		System.out.println("Vida: " + this.hp);
+		System.out.println("Armadura: " + this.armadura);
+		System.out.println("Resistência Mágica: " + this.resisteciaMagica);
+		System.out.println("Dano Físico: " + this.danoFisico);
+		System.out.println("Dano Mágico: " + this.danoMagico);
+		System.out.println("Crítico: " + this.critico + "\n");
+
 	}
-	public void mostrarStatus() 
-	{
-		if (ocultar == true) 
-		{
-			System.out.println("Status base do jogador "+nome+" estão ocultos \n");
-		}
-		else 
-		{
+
+	public void mostrarStatus() {
+		if (ocultar == true) {
+			System.out.println("Status base do jogador " + nome + " estão ocultos \n");
+		} else {
 			status();
 		}
-	
-		
+
 	}
-	public void invisivel()
-	{
+
+	public void invisivel() {
 		this.ocultar = true;
 	}
-	
-	public void visivel()
-	{
+
+	public void visivel() {
 		this.ocultar = false;
 	}
 
-	public void nome ()
-	{
-			
-		System.out.println("Digite o nome do jogador ");
+	// metodos das skills
+	public void Santouryuu() {
+		System.out.println("Santouryuu " + (santouryuu-armadura) + " de dano");
+		danoFisico += 15;
+		hp = hp - santouryuu + armadura;
+		if (danoFisico>=44 && danoFisico<=46) {
+			critico += (float) 0.5;
+		}
 	}
+
+	public void Bakuhatsu() {
+		
+		if (cont >0) {
+			System.out.println("Bakuhatsu " + (bakuhatsu * critico+(danoFisico+danoMagico)) + " de dano ");
+			System.out.println((cont-1)+"/5 restantes");
+			hp = hp - (bakuhatsu * critico+(danoFisico+danoMagico));
+			cont--;
+		}
+		
+		if (cont == 0) {
+			System.out.println("Bakuhatsu não está mais disponível");
+		}
+	}
+
+	public void TotsukaTsurugi() {
+		System.out.println("TotsukaTsurugi " + (totsukaTsurugi-armadura) + " de dano");
+		danoMagico += 9;
+		hp = hp - totsukaTsurugi + armadura;
+		if(danoMagico>=31 && danoMagico<=33) {
+			totsukaTsurugi += danoMagico;
+		}
+		
+	}
+
+	public void FaiyaaArashi() {
+		System.out.println("Faiyaa Arashi " + (faiyaaArashi-resisteciaMagica) + " de dano");
+		hp = hp - faiyaaArashi + resisteciaMagica;
+		if (hp <= 900 && hp > 700) {
+			int x;
+			for (x = 3; x <= 9; x++) {
+				faiyaaArashi += x;
+			}
+		}
+
+		if (hp <= 700 && hp > 500) {
+			int x;
+			for (x = 3; x <= 6; x++) {
+				faiyaaArashi += x;
+			}
+		}
+
+		if (hp <= 300) {
+			int x;
+			for (x = 3; x <= 3; x++) {
+				faiyaaArashi += x;
+			}
+		}
+
+	}
+
+	public void Blizzard() {
+		System.out.println("Blizzard " + (blizzard-resisteciaMagica) + " de dano");
+		hp = hp - blizzard + resisteciaMagica;
+		resisteciaMagica -= 1;
+			
+		if (resisteciaMagica == 0) {
+			resisteciaMagica = 3;
+		}
+	}
+
+	public void Raitou() {
+		System.out.println("Raitou acumulou  "+(raitou+raitou)+" de dano "+ac+"/5 acumulos");
+		
+		if(ac<=5) {
+			ac++;
+
+		}
+		if (ac==6) {
+			ac=1;
+		}
+
+		
+		if (raitou<128) {
+				raitou = raitou + raitou;
+				hp = hp - 0;
+		}
+
+		if(raitou == 128) {
+			System.out.println("Raitou causou "+ (raitou-resisteciaMagica)+" de dano acumulado");
+			hp = hp - raitou + resisteciaMagica;
+			raitou = 8;
+		}
+	}
+
 	
+	// getters e setters
+
+	public int getAc() {
+		return ac;
+	}
+
+	public void setAc(int ac) {
+		this.ac = ac;
+	}
+
+	public float getFaiyaaArashi() {
+		return faiyaaArashi;
+	}
+
+	public void setFaiyaaArashi(float faiyaaArashi) {
+		this.faiyaaArashi = faiyaaArashi;
+	}
+
+	public int getBlizzard() {
+		return blizzard;
+	}
+
+	public void setBlizzard(int blizzard) {
+		this.blizzard = blizzard;
+	}
+
+	public int getRaitou() {
+		return raitou;
+	}
+
+	public void setRaitou(int raitou) {
+		this.raitou = raitou;
+	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	public float getHP() {
-		return HP;
+
+	public float getHp() {
+		return hp;
 	}
-	public void setHP(float hP) {
-		HP = hP;
+
+	public void setHp(float hp) {
+		this.hp = hp;
 	}
+
+	public float getBakuhatsu() {
+		return bakuhatsu;
+	}
+
+	public void setBakuhatsu(float bakuhatsu) {
+		this.bakuhatsu = bakuhatsu;
+	}
+
+	public int getTotsukaTsurugi() {
+		return totsukaTsurugi;
+	}
+
+	public void setTotsukaTsurugi(int totsukaTsurugi) {
+		this.totsukaTsurugi = totsukaTsurugi;
+	}
+
 	public int getArmadura() {
 		return armadura;
 	}
+
 	public void setArmadura(int armadura) {
 		this.armadura = armadura;
 	}
+
 	public int getResisteciaMagica() {
 		return resisteciaMagica;
 	}
+
 	public void setResisteciaMagica(int resisteciaMagica) {
 		this.resisteciaMagica = resisteciaMagica;
 	}
+
 	public int getDanoFisico() {
 		return danoFisico;
 	}
+
 	public void setDanoFisico(int danoFisico) {
 		this.danoFisico = danoFisico;
 	}
+
 	public int getDanoMagico() {
 		return danoMagico;
 	}
+
 	public void setDanoMagico(int danoMagico) {
 		this.danoMagico = danoMagico;
 	}
-	public double getCritico() {
+
+	public float getCritico() {
 		return critico;
 	}
-	public void setCritico(double critico) {
+
+	public void setCritico(float critico) {
 		this.critico = critico;
 	}
+
 	public boolean isOcultar() {
 		return ocultar;
 	}
+
 	public void setOcultar(boolean ocultar) {
 		this.ocultar = ocultar;
 	}
-	
+
+	public int getSantouryuu() {
+		return santouryuu;
+	}
+
+	public void setSantouryuu(int santouryuu) {
+		santouryuu = santouryuu;
+	}
+
 }
